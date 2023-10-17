05b44b8c2959d6ba56b15e6a355c2013dff5f83e
Cendos88 <cendos88@gmail.com>
2023-10-17 22:08:55 +0200

    configured db and created user entity

---
+++ pub/src/main/java/com/pub/models/User.java
@@ -0,0 +1,25 @@
+package com.pub.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private boolean isActive;
+    private boolean idAdult;
+    private int Pocket;
+}
