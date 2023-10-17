0d040797d8b446c7d64ea553d869af23389bf4c3
Cendos88 <cendos88@gmail.com>
2023-10-17 23:23:45 +0200

    added more methods to jwt Service

--- pub/src/main/java/com/pub/services/JwtTokenService.java
+++ pub/src/main/java/com/pub/services/JwtTokenService.java
@@ -2,17 +2,26 @@ package com.pub.services;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtTokenService {
     private static final String secret = "11b9bfb2d5ea462d8a8f73454b2a4c385d592504934d15432c6d5dfb2ddb8db9";
     public String extractName(String jwt) {
-        return null;
+        return extractClaim(jwt, Claims::getSubject);
+    }
+    public <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwt);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String jwt) {
@@ -26,4 +35,32 @@ public class JwtTokenService {
         byte[] secretBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(secretBytes);
     }
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(Map.of(), userDetails);
+    }
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 * 60))
+                .signWith(getSighInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public boolean isTokenValid(String jwt, UserDetails userDetails) {
+        final String name = extractName(jwt);
+        return (name.equals(userDetails.getUsername()) && !isTokenExpired(jwt));
+    }
+    public boolean isTokenExpired(String jwt) {
+        return extractExpiration(jwt).before(new Date());
+    }
+
+    private Date extractExpiration(String jwt) {
+        return extractClaim(jwt, Claims::getExpiration);
+    }
+
 }
